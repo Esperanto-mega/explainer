ecd705c5267e0a7f6b4773955882f44f0f1b6341
Esperanto-mega <57856207+Esperanto-mega@users.noreply.github.com>
2024-04-13 19:10:16 +0800

    Add files via upload

---
+++ model_training.py
@@ -0,0 +1,119 @@
+# yinjun@2024/04/13
+
+import argparse
+import numpy as np
+from tqdm import tqdm
+
+import torch
+from torch import Tensor
+
+from torch_geometric.nn import GCNConv
+from torch_geometric.datasets import BA2MotifDataset
+from torch_geometric.loader.dataloader import DataLoader
+
+# Command arguments
+parser = argparse.ArgumentParser(description = 'PGExplainer')
+parser.add_argument("--seed", type = int, default = 42, help = "Random seed")
+parser.add_argument("--data_path", type = str, default = '', help = "Root directory where the dataset should be saved")
+parser.add_argument("--batch_size", type = int, default = 64, help = "")
+parser.add_argument("--device", type = str, default = 'cuda:0', help = "")
+parser.add_argument("--model_path", type = str, default = '', help = "Root directory where the trained model should be saved")
+parser.add_argument("--hidden_dim", type = int, default = 32, help = "")
+parser.add_argument("--lr", type = float, default = 5e-3, help = "")
+parser.add_argument("--epochs", type = int, default = 30, help = "")
+parser.add_argument("--eval_step", type = int, default = 5, help = "")
+args = parser.parse_args()
+
+device = torch.device(args.device)
+
+# Dataset
+dataset = BA2MotifDataset(args.data_path)
+'''
+dataset: BA2MotifDataset(1000)
+dataset[0]: Data(x=[25, 10], edge_index=[2, 50], y=[1])
+dataset.num_features: 10
+dataset.num_classes: 2
+'''
+# Dataloader
+train_num = 0.8 * len(dataset)
+valid_num = 0.9 * len(dataset)
+trainset, validset, testset = dataset[:train_num], dataset[train_num:valid_num], dataset[valid_num:]
+trainloader = DataLoader(trainset, batch_size = args.batchsize, shuffle = True)
+validloader = DataLoader(validset, batch_size = args.batchsize, shuffle = True)
+testloader = DataLoader(testset, batch_size = 1, shuffle = False)
+
+# GNN model
+# Define a simple GCN.
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+
+    def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
+        # x: Node feature matrix of shape [num_nodes, in_channels]
+        # edge_index: Graph connectivity matrix of shape [2, num_edges]
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index)
+        return x
+
+model = GCN(dataset.num_features, args.hidden_dim, dataset.num_classes)
+model = model.to(device)
+model.train()
+
+optimizer = torch.optim.Adam(model.parameters(), lr = args.lr)
+loss_fn = torch.nn.BCELoss()
+
+best_accuracy = 0.0
+# Train
+for epoch in range(args.epochs):
+    total_correct = []
+    for data in tqdm(trainloader):
+        data = data.to(device)
+        # data.shape: (num_nodes, num_features)
+        optimizer.zero_grad()
+        output = model(data)
+        # output.shape: (batchsize, num_classes)
+        label = data.y
+        # label.dim: (batchsize)
+        loss = loss_fn(output, label)
+        loss.backward()
+        
+        prediction = torch.argmax(output, dim = 1)
+        # prediction.shape: (batchsize)
+        correct = prediction == label
+        total_correct.append(correct.detach().cpu().numpy().tolist())
+    accuracy = np.sum(total_correct) / len(trainset)
+
+    # Validation
+    if (epoch + 1) % args.eval_step == 0:
+        with torch.no_grad():
+            val_total_correct = []
+            for data in tqdm(validloader):
+                data = data.to(device)
+                output = model(data)
+                label = data.y
+                val_loss = loss_fn(output, data.y)
+
+                prediction = torch.argmax(output, dim = 1)
+                val_correct = prediction == label
+                val_total_correct.append(val_correct.detach().cpu().numpy().tolist())
+            val_accuracy = np.sum(val_total_correct) / len(validset)
+
+            if val_accuracy > best_accuracy:
+                torch.save(model.state_dict(), args.model_path)
+
+# Evaluate
+eval_model = GCN(dataset.num_features, args.hidden_dim, dataset.num_classes)
+eval_model.load_state_dict(args.model_path)
+eval_model = eval_model.to(device)
+eval_model.eval()
+eval_total_correct = []
+for data in tqdm(testloader):
+    data = data.to(device)
+    output = model(data)
+    label = data.y
+    prediction = torch.argmax(output, dim = 1)
+    eval_correct = prediction == label
+    eval_total_correct.append(eval_correct.detach().cpu().numpy().tolist())
+eval_accuracy = np.sum(eval_total_correct) / len(testset)
