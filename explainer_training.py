3d9fb1a9901ae6ee9e5fbcc934294d0d8b677211
Esperanto-mega <57856207+Esperanto-mega@users.noreply.github.com>
2024-04-25 18:41:54 +0800

    Update explainer_training.py

--- explainer_training.py
+++ explainer_training.py
@@ -1,17 +1,16 @@
-# yinjun@2024/04/13
+# @yinjun
 
 import random
 import argparse
 from tqdm import tqdm
+import numpy as np
 
 import torch
-
 from torch_geometric.nn import GCNConv
 from torch_geometric.datasets import BA2MotifDataset
 from torch_geometric.explain import Explainer, PGExplainer
-from torch_geometric.explain.metric import fidelity
+from torch_geometric.explain.metric import fidelity, unfaithfulness
 from torch_geometric.loader.dataloader import DataLoader
-
 
 from GCN import GraphGCN
 
@@ -21,8 +20,7 @@ parser.add_argument("--seed", type = int, default = 42, help = "Random seed")
 parser.add_argument("--data_path", type = str, default = '', help = "Root directory where the dataset should be saved")
 parser.add_argument("--device", type = str, default = 'cuda:0', help = "")
 parser.add_argument("--model_path", type = str, default = '', help = "Model to be explained")
-parser.add_argument("--train_ratio", type = float, default = 0.7, help = "")
-parser.add_argument("--valid_ratio", type = float, default = 0.1, help = "")
+parser.add_argument("--train_ratio", type = float, default = 0.9, help = "")
 parser.add_argument("--epochs", type = int, default = 30, help = "Explainer training epochs")
 parser.add_argument("--eval_step", type = int, default = 5, help = "Explainer validation steps")
 parser.add_argument("--lr", type = float, default = 5e-3, help = "Explainer learning rate")
@@ -62,11 +60,9 @@ device = torch.device(args.device)
 dataset = BA2MotifDataset(args.data_path)
 index = list(range(len(dataset)))
 random.shuffle(index)
-train_num = round(0.8 * len(dataset))
-valid_num = round(0.9 * len(dataset))
-trainset, validset, testset = dataset[index[:train_num]], dataset[index[train_num:valid_num]], dataset[index[valid_num:]]
+train_num = round(args.train_ratio * len(dataset))
+trainset, testset = dataset[index[:train_num]], dataset[index[train_num:]]
 trainloader = DataLoader(trainset, batch_size = args.batch_size, shuffle = True)
-validloader = DataLoader(validset, batch_size = args.batch_size, shuffle = True)
 testloader = DataLoader(testset, batch_size = 1, shuffle = False)
 
 # GNN model to be explained
@@ -93,6 +89,7 @@ explainer = Explainer(
 
 # Explainer training
 for epoch in range(args.epochs):
+    total_loss = 0
     for data in tqdm(trainloader):
         data = data.to(device)
         target = explained_model(data.x, data.edge_index, data.batch)
@@ -104,9 +101,28 @@ for epoch in range(args.epochs):
             batch = data.batch,
             target = target
         )
-        explanation = explainer(batch.x, batch.edge_index)
-        fidelity = fidelity(explainer, explanation)
-        print('fidelity:', fidelity)
-#explanation = explainer(x = data.x, edge_index = data.edge_index, batch = data.batch, target = target)
-#fidelity = fidelity(explainer, explanation)
-#print('epoch',epoch+1,'fidelity:', fidelity)
+        total_loss += explain_loss
+    total_loss /= train_num
+    print('epoch', epoch + 1,'loss:', total_loss)
+
+# Explainer evaluating
+unfaithful_list = []
+fid_pos_list = []
+fid_neg_list = []
+for data in tqdm(testloader):
+    data = data.to(device)
+    target = explained_model(data.x, data.edge_index, data.batch)
+    explanation = explainer(x = data.x, edge_index = data.edge_index, batch = data.batch, target = target)
+    fid = fidelity(explainer, explanation)
+    unfaithful = unfaithfulness(explainer, explanation)
+    fid_pos_list.append(fid[0])
+    fid_neg_list.append(fid[1])
+    unfaithful_list.append(unfaithful)
+    
+
+fid_pos = np.mean(fid_pos_list)
+fid_neg = np.mean(fid_neg_list)
+unfaithful = np.mean(unfaithful_list)
+print('fid_pos:', fid_pos)
+print('fid_neg:', fid_neg)
+print('unfaithfulness:', unfaithful)
