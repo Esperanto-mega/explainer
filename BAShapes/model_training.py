cf5a759e8a857845b2db87fe74e26f8ea6373b06
Esperanto-mega <57856207+Esperanto-mega@users.noreply.github.com>
2024-04-26 16:08:50 +0800

    Update model_training.py

--- BAShapes/model_training.py
+++ BAShapes/model_training.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torch.nn.functional as F
 from sklearn.metrics import roc_auc_score
@@ -11,23 +13,40 @@ from torch_geometric.explain import Explainer, GNNExplainer
 from torch_geometric.nn import GCN
 from torch_geometric.utils import k_hop_subgraph
 
+parser = argparse.ArgumentParser(description = 'BAShapes')
+parser.add_argument("--device", type = str, default = 'cuda:0', help = "")
+parser.add_argument("--lr", type = float, default = 0.001, help = "")
+parser.add_argument("--weight_decay", type = float, default = 0.005, help = "")
+parser.add_argument("--train_size", type = float, default = 0.8, help = "")
+parser.add_argument("--hidden_dim", type = int, default = 20, help = "")
+parser.add_argument("--num_layers", type = int, default = 3, help = "")
+parser.add_argument("--num_nodes", type = int, default = 300, help = "The number of nodes.")
+parser.add_argument("--num_edges", type = int, default = 5, help = "The number of edges from a new node to existing nodes.")
+parser.add_argument("--num_motifs", type = int, default = 80, help = "The number of motifs to attach to the graph.")
+
+args = parser.parse_args()
+print(args)
+
+# torch_geometric.transforms.Constant - Appends a constant value (default: 1.0) to each node feature.
 dataset = ExplainerDataset(
-    graph_generator=BAGraph(num_nodes=300, num_edges=5),
-    motif_generator='house',
-    num_motifs=80,
-    transform=T.Constant(),
+    graph_generator = BAGraph(num_nodes = args.num_nodes, num_edges = args.num_edges),
+    motif_generator = 'house',
+    num_motifs = args.num_motifs,
+    transform = T.Constant(),
 )
 data = dataset[0]
 
 idx = torch.arange(data.num_nodes)
-train_idx, test_idx = train_test_split(idx, train_size=0.8, stratify=data.y)
+train_idx, test_idx = train_test_split(idx, train_size = args.train_size, stratify = data.y)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device(args.device)
 data = data.to(device)
-model = GCN(data.num_node_features, hidden_channels=20, num_layers=3,
-            out_channels=dataset.num_classes).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.005)
-
+model = GCN(
+    data.num_node_features,
+    hidden_channels = args.hidden_dim,
+    num_layers = args.num_layers,
+    out_channels = dataset.num_classes).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr = args.lr, weight_decay = args.weight_decay)
 
 def train():
     model.train()
@@ -38,7 +57,6 @@ def train():
     loss.backward()
     optimizer.step()
     return float(loss)
-
 
 @torch.no_grad()
 def test():
@@ -52,7 +70,6 @@ def test():
     test_acc = test_correct / test_idx.size(0)
 
     return train_acc, test_acc
-
 
 pbar = tqdm(range(1, 2001))
 for epoch in pbar:
