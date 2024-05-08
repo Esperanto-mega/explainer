d5e484e6391b9c90ffe1af1015cd0a181af13caa
Esperanto-mega <57856207+Esperanto-mega@users.noreply.github.com>
2024-05-08 15:16:25 +0800

    Update GCN.py

--- Mutag/GCN.py
+++ Mutag/GCN.py
@@ -5,6 +5,7 @@ from torch.nn import ModuleList
 from torch.nn import Sequential
 from torch.nn import ReLU, Linear, Softmax
 from torch_geometric.nn import GCNConv, global_max_pool, global_mean_pool
+from torch_geometric.nn import GINEConv, BatchNorm
 
 class MutagNet(torch.nn.Module):
     def __init__(self, conv_unit = 2):
@@ -59,32 +60,3 @@ class MutagNet(torch.nn.Module):
         with torch.no_grad():
             for param in self.parameters():
                 param.uniform_(-1.0, 1.0)
-
-class GraphGCN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels):
-        super().__init__()
-        self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, hidden_channels)
-        self.conv3 = GCNConv(hidden_channels, hidden_channels)
-        self.linear = torch.nn.Linear(hidden_channels, out_channels)
-        # self.linear = torch.nn.Linear(hidden_channels * 2, out_channels)
-
-    def forward(self, x, edge_index, batch):
-        x = self.conv1(x, edge_index)
-        x = torch.nn.functional.normalize(x, p = 2, dim = 1)
-        x = x.relu()
-      
-        x = self.conv2(x, edge_index)
-        x = torch.nn.functional.normalize(x, p = 2, dim = 1)
-        x = x.relu()
-      
-        x = self.conv3(x, edge_index)
-        x = torch.nn.functional.normalize(x, p = 2, dim = 1)
-        x = x.relu()
-      
-        x_mean = global_mean_pool(x, batch)
-        # x_max = global_max_pool(x, batch)
-        # x = torch.cat([x_mean, x_max], dim = -1)
-        
-        x = self.linear(x)
-        return x
